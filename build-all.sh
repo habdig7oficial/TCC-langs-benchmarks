e5ad36630469596629cd502d764f781b2f2aa9c1
Mateus Felipe da Silveira VIeira <65318468+habdig7oficial@users.noreply.github.com>
2023-04-24 18:23:17 +0000

    Dockerising

---
+++ build-all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+## Rust - Actix
+cd rust-actix
+docker build -t rust-actix .
+cd ..
+
+## TS (node) - Express
+cd node-ts-express
+docker build -t node-express .
+cd ..
+
+## TS (deno) - Oak
+
+#docker build -t deno-oak
+
+## Go - Gin
+cd go-gin
+docker build -t go-gin .
+cd ..
--- c++-drogon/main.cc
+++ c++-drogon/main.cc
@@ -1,10 +1,14 @@
 #include <drogon/drogon.h>
+
 int main() {
     //Set HTTP listener address and port
     drogon::app().addListener("0.0.0.0",8080);
+    drogon::app().setThreadNum(0);
     //Load config file
     //drogon::app().loadConfigFile("../config.json");
     //Run HTTP framework,the method will block in the internal event loop
+
+    cout << "C++ - Drogon";
     drogon::app().run();
     return 0;
 }
